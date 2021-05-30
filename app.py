fa9d92cd36bb5adaab0b1542dc810818193a5dda
eswaraprasad001 <eswaraprasad172001@gmail.com>
2021-05-30 15:15:19 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,145 @@
+import torch
+import torch.nn as nn
+from torchvision.transforms import transforms
+import numpy as np
+from torch.autograd import Variable
+from torchvision.models import squeezenet1_1
+import torch.functional as F
+from io import open
+import os
+from PIL import Image
+import pathlib
+import glob
+import cv2
+from flask import Flask
+from flask import request
+from flask import render_template
+
+pred_path='/home/thor_001/ML/DL_Deploy/images'
+UPLOAD_FOLDER='/home/thor_001/ML/DL_Deploy/images'
+
+app = Flask(__name__)
+classes=['Junk',"Not a Junk"]
+
+
+class ConvNet(nn.Module):
+    def __init__(self,num_classes=2):
+        super(ConvNet,self).__init__()
+        
+        #Output size after convolution filter
+        #((w-f+2P)/s) +1
+        
+        #Input shape= (256,3,150,150)
+        
+        self.conv1=nn.Conv2d(in_channels=3,out_channels=12,kernel_size=3,stride=1,padding=1)
+        #Shape= (256,12,150,150)
+        self.bn1=nn.BatchNorm2d(num_features=12)
+        #Shape= (256,12,150,150)
+        self.relu1=nn.ReLU()
+        #Shape= (256,12,150,150)
+        
+        self.pool=nn.MaxPool2d(kernel_size=2)
+        #Reduce the image size be factor 2
+        #Shape= (256,12,75,75)
+        
+        
+        self.conv2=nn.Conv2d(in_channels=12,out_channels=20,kernel_size=3,stride=1,padding=1)
+        #Shape= (256,20,75,75)
+        self.relu2=nn.ReLU()
+        #Shape= (256,20,75,75)
+        
+        
+        
+        self.conv3=nn.Conv2d(in_channels=20,out_channels=32,kernel_size=3,stride=1,padding=1)
+        #Shape= (256,32,75,75)
+        self.bn3=nn.BatchNorm2d(num_features=32)
+        #Shape= (256,32,75,75)
+        self.relu3=nn.ReLU()
+        #Shape= (256,32,75,75)
+        
+        
+        self.fc=nn.Linear(in_features=75 * 75 * 32,out_features=num_classes)
+        
+        
+        
+        #Feed forwad function
+        
+    def forward(self,input):
+        output=self.conv1(input)
+        output=self.bn1(output)
+        output=self.relu1(output)
+            
+        output=self.pool(output)
+            
+        output=self.conv2(output)
+        output=self.relu2(output)
+            
+        output=self.conv3(output)
+        output=self.bn3(output)
+        output=self.relu3(output)
+            
+            
+            #Above output will be in matrix form, with shape (256,32,75,75)
+            
+        output=output.view(-1,32*75*75)
+            
+            
+        output=self.fc(output)
+            
+        return output
+
+
+checkpoint=torch.load('Junkornot.pth')
+model=ConvNet(num_classes=10)
+model.load_state_dict(checkpoint, strict=False)
+model.eval()
+
+transformer=transforms.Compose([
+    transforms.Resize((150,150)),
+    transforms.ToTensor(),  #0-255 to 0-1, numpy to tensors
+    transforms.Normalize([0.5,0.5,0.5], # 0-1 to [-1,1] , formula (x-mean)/std
+                        [0.5,0.5,0.5])
+])
+
+def prediction(img_path,transformer):
+    
+    image=Image.open(img_path)
+    
+    image_tensor=transformer(image).float()
+    
+    
+    image_tensor=image_tensor.unsqueeze_(0)
+    
+    if torch.cuda.is_available():
+        image_tensor.cuda()
+        
+    input=Variable(image_tensor)
+    
+    
+    output=model(input)
+    
+    index=output.data.numpy().argmax()
+    
+    pred=classes[index]
+    
+    return pred
+
+
+@app.route("/", methods=["GET", "POST"])
+def upload_predict():
+    if request.method == "POST":
+        image_file = request.files["image"]
+        if image_file:
+            image_location = os.path.join(
+                UPLOAD_FOLDER,
+                image_file.filename
+            )
+            image_file.save(image_location)
+
+            pred = prediction(image_location, transformer)
+            
+            return render_template("index.html", prediction=pred, image_loc=image_file.filename)
+    return render_template("index.html", prediction=0, image_loc=None)
+if __name__ == "__main__":
+
+    app.run(host="127.0.0.1",debug=False)
